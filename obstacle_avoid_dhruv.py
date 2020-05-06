e4e72d6ea8e9b0274c83bb9d9a287fce13cc14ed
Manofmomo <bhatiadhruv2001@gmail.com>
2020-05-06 13:40:54 +0530

    Bug fixes

--- obstacle_avoid_dhruv.py
+++ obstacle_avoid_dhruv.py
@@ -3,24 +3,27 @@ import rospy
 from geometry_msgs.msg import TwistStamped
 from sensor_msgs.msg import LaserScan
 def callback_lidar(msg):
-    global lidar_range
-    lidar_range = msg
+    counter =0 
+    for i in range(0,720):
+        if msg.ranges[i]>msg.range_min and msg.ranges[i]<msg.range_max:
+            if msg.ranges[i]<10:
+                rospy.loginfo("object detected")
+                counter=1
+                stop_now()
+    if counter == 0:
+        print("All Clear")
+
 
 
 def oa_detect():
-    rospy.init_node("OA detector",anonymous=True)
+    rospy.init_node("OA",anonymous=True)
     rospy.Subscriber('/spur/laser/scan',LaserScan,callback_lidar)
-    for i in range(0,720):
-        if lidar_range.ranges[i]>msg.range_min and lidar_range.range[i]<range_max:
-            if lidar_range.range[i]<1:
-                stop_now()
-            
     rospy.spin()
                 
 
 
 def stop_now():
-    pub_vel=rospy.Pubisher('/mavros/setpoint_velocity/cmd_vel',TwistStamped,queue_size=20)
+    pub_vel=rospy.Publisher('/mavros/setpoint_velocity/cmd_vel',TwistStamped,queue_size=20)
     vel=TwistStamped()
     vel.twist.linear.x=0
     vel.twist.linear.y=0
@@ -35,5 +38,5 @@ if __name__=="__main__":
         while not rospy.is_shutdown():
             oa_detect()
 
-    except rospy.RosInterruptException:
+    except rospy.ROSInterruptException:
         pass
